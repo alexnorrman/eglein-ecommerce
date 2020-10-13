13eb0d756640c5fed5bf71f33a83093656776973
Alex Norrman <alexnorrman@gmail.com>
2020-10-13 10:59:58 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
+/* Add config! */
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyC2EmyKqhIbBY7PRUuwuEUUTkkrLG4FLaE",
-    authDomain: "eglein-16e44.firebaseapp.com",
-    databaseURL: "https://eglein-16e44.firebaseio.com",
-    projectId: "eglein-16e44",
-    storageBucket: "eglein-16e44.appspot.com",
-    messagingSenderId: "223442958425",
-    appId: "1:223442958425:web:381b9105f701318f0f9948",
-    measurementId: "G-1JDJ7R7C6Y"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 const db = firebaseConfig.firestore();
